f062af45049409af2a9e033a63e3c9f2c04a265b
Eunbin Bae <81680796+eungbean1029@users.noreply.github.com>
2024-07-21 22:32:56 +0900

    feat: s3Mock 테스트코드 추가

---
+++ src/test/java/com/eungbean/example/s3Test/s3MockTest/config/TestAwsS3MockConfig.java
@@ -0,0 +1,32 @@
+package com.eungbean.example.s3Test.s3MockTest.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.AnonymousAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import io.findify.s3mock.S3Mock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//@Configuration
+public class TestAwsS3MockConfig {
+
+    private final int port = 9000;
+
+    @Bean
+    public S3Mock s3Mock() {
+        return new S3Mock.Builder().withPort(port).withInMemoryBackend().build();
+    }
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        String endpoint = "http://localhost:" + port;
+        return AmazonS3ClientBuilder
+                .standard()
+                .withPathStyleAccessEnabled(true)
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, Regions.US_EAST_1.name()))
+                .withCredentials(new AWSStaticCredentialsProvider(new AnonymousAWSCredentials()))
+                .build();
+    }
+}
